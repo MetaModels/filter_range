8564de26a0cda162113695cd50260d19ec4c14de
Mini Model <minimodel@metamodel.me>
2013-07-01 12:16:56 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsfilter_range/languages/fr/tl_metamodel_filtersetting.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T07:59:00+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['0'] = 'Champ pour la valeur 1';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['1'] = 'Afficher un champ front office pour la valeur 1.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['0'] = 'Valeur 2 incluse';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['1'] = 'Exclu par défaut.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['0'] = 'Valeur 1 incluse';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['1'] = 'Exclu par défaut.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['0']   = 'Champ pour la valeur 2';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['1']   = 'Afficher un champ front office pour la valeur 2.';
