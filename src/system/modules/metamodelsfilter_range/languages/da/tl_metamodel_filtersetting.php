f5913e02b5785d787eae826e0e5dcd035d7da2a3
Mini Model <minimodel@metamodel.me>
2013-07-06 00:15:44 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsfilter_range/languages/da/tl_metamodel_filtersetting.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/da/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T07:59:00+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['0']      = 'Anden egenskab';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['1']      = 'Anden egenskab, denne indstilling relaterer til.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['0']     = 'Felt for værdi 1';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['1']     = 'Vis frontend felt for værdi 1.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['0']     = 'Værdi 2 inkluderet';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['1']     = 'Standard: ekskluderet.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['0']     = 'Værdi 1 inkluderet';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['1']     = 'Standard: ekskluderet.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['0']       = 'Felt for værdi 2';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['1']       = 'Vis frontend felt for værdi 2.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['range'] = 'Værdien imellem to felter';
