58de4f59be6d61c8626f2c3ce97c90b498007205
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-02-18 01:17:22 +0100

    Add filter setting type factory.

---
+++ src/MetaModels/Filter/Setting/RangeFilterSettingTypeFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage FilterRange
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\Filter\Setting;
+
+/**
+ * Attribute type factory for from-to filter settings.
+ */
+class RangeFilterSettingTypeFactory extends AbstractFilterSettingTypeFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this
+            ->setTypeName('fromto')
+            ->setTypeIcon('system/modules/metamodelsfilter_range/html/filter_range.png')
+            ->setTypeClass('MetaModels\Filter\Setting\Range')
+            ->allowAttributeTypes('numeric', 'decimal');
+    }
+}
