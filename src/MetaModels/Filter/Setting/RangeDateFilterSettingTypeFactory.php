799986aea0df380c84da34be0ec77cbd430f425d
Stefan Heimes <stefan_heimes@hotmail.com>
2015-06-05 20:24:11 +0200

    Add a new TypeFactory for the new range class.

---
+++ src/MetaModels/Filter/Setting/RangeDateFilterSettingTypeFactory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage FilterRange
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\Filter\Setting;
+
+/**
+ * Attribute type factory for from-to filter settings.
+ */
+class RangeDateFilterSettingTypeFactory extends AbstractFilterSettingTypeFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this
+            ->setTypeName('rangedate')
+            ->setTypeIcon('system/modules/metamodelsfilter_range/html/filter_range.png')
+            ->setTypeClass('MetaModels\Filter\Setting\RangeDate')
+            ->allowAttributeTypes('numeric', 'decimal', 'timestamp');
+    }
+}
