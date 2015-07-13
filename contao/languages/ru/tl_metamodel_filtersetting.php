deca9ad89fc6f94e751d24a1d5ed6953b9e391f2
Mini Model <minimodel@metamodel.me>
2015-07-13 12:15:26 +0200

    Updated translations from transifex.

--- contao/languages/ru/tl_metamodel_filtersetting.php
+++ contao/languages/ru/tl_metamodel_filtersetting.php
@@ -8,9 +8,8 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-12-22T16:14:21+01:00
+ * last-updated: 2015-07-11T08:02:28+02:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['0']            = 'Второй атрибут';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['1']            = 'Второй атрибут, этот параметр относится к.';
@@ -24,7 +23,9 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['0']            = 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['1']            = 'Вы можете выбрать желаемую схему.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['date']  = 'Дата';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['datim'] = 'Дата и время';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['time']  = 'Время';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['0']             = 'Поле для значения 2';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['1']             = 'Показать FE-поле для значения 2.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['range']       = 'Значение в 2 областях';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['rangedate']   = 'Значение в 2 областях для даты';
 
