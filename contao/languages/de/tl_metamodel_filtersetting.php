23bf16aa43b21338465c23e55615c64387fc6dbb
Mini Model <minimodel@metamodel.me>
2015-11-11 12:16:22 +0100

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_filtersetting.php
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -8,17 +8,17 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T08:02:28+02:00
+ * last-updated: 2015-09-18T12:15:50+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['0']            = 'Zweites Attribut';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['1']            = 'Das Zweite Attribut auf das sich die Einstellung bezieht.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['0']           = 'Feld für Wert 1';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['1']           = 'Zeige Frontend-Feld für Wert 1.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['0']           = 'Wert 2 einfügen';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['1']           = 'Standard: ausgeschlossen.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['0']           = 'Wert 1 einfügen';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['1']           = 'Standard: ausgeschlossen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['0']           = 'Wert 2 einbeziehen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['1']           = 'Standard: Wert nicht eingeschlossen.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['0']           = 'Wert 1 einbeziehen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['1']           = 'Standard: Wert nicht eingeschlossen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['0']            = 'Schema';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['1']            = 'Hier können die das gewünschte Schema auswählen.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['date']  = 'Datum';
@@ -26,6 +26,6 @@ $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['datim'] = 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['time']  = 'Zeit';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['0']             = 'Feld für Wert 2';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['tofield']['1']             = 'Zeige Frontend-Feld für Wert 2.';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['range']       = '2 Felder mit Werten';
-$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['rangedate']   = 'Wert mit zwei Feldern für das Datum.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['range']       = 'Wert von/bis für zwei Felder';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['rangedate']   = 'Wert von/bis für zwei Datumsfelder';
 
