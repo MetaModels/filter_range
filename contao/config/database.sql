c9fc5bb6748988494d7e5186e7d8074c581bab3b
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-01-13 18:40:02 +0100

    Fix issue #3 - add missing columns to database.sql

--- contao/config/database.sql
+++ contao/config/database.sql
@@ -19,5 +19,7 @@ CREATE TABLE `tl_metamodel_filtersetting` (
   `moreequal` char(1) NOT NULL default '1',
   `lessequal` char(1) NOT NULL default '1',
   `fromfield` char(1) NOT NULL default '1',
-  `tofield` char(1) NOT NULL default '1'
+  `tofield` char(1) NOT NULL default '1',
+  `dateformat` char(32) NOT NULL default '',
+  `timetype` varchar(64) NOT NULL default ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
