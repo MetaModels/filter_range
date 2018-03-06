968b1927f389ad81934f3fc0d8bd49d25db485e3
Mini Model <minimodel@metamodel.me>
2018-03-06 13:23:37 +0100

    Updated translations from transifex.

---
+++ contao/languages/et/tl_metamodel_filtersetting.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2018-03-06T13:23:36+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['attr_id2']['0']            = 'Teine omadus';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['fromfield']['0']           = 'Väärtuse 1 väli';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['lessequal']['1']           = 'Standard: välja jäetud.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['moreequal']['1']           = 'Standard: välja jäetud.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetype']['0']            = 'Skeem';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['date']  = 'Kuupäev';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['datim'] = 'Kuupäev ja kellaaeg';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['timetypeOptions']['time']  = 'Kellaaeg';
+
