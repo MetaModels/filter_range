d7b70d75a1e01862c670056e271f072b31b69cb7
Sven Baumann <baumann.sv@gmail.com>
2019-04-03 17:13:47 +0200

    Add the missing filter setting type renderer

---
+++ src/EventListener/DcGeneral/Table/FilterSetting/RangeFilterSettingTypeRendererListener.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of MetaModels/filter_range.
+ *
+ * (c) 2012-2019 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/filter_range
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/filter_range/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+namespace MetaModels\FilterRangeBundle\EventListener\DcGeneral\Table\FilterSetting;
+
+use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
+use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
+use MetaModels\CoreBundle\EventListener\DcGeneral\Table\FilterSetting\AbstractFilterSettingTypeRenderer;
+
+/**
+ * Handles rendering of model from tl_metamodel_filtersetting.
+ */
+class RangeFilterSettingTypeRendererListener extends AbstractFilterSettingTypeRenderer
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTypes()
+    {
+        return ['range', 'rangedate'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getLabelParameters(EnvironmentInterface $environment, ModelInterface $model)
+    {
+        return $this->getLabelParametersWithAttributeAndUrlParam($environment, $model);
+    }
+}
