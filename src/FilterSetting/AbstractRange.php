2c5419f258dbf812ab05276b27dd5758209734e9
Ingolf Steinhardt <info@e-spin.de>
2025-06-17 09:47:44 +0200

    Fix error fit value empty

--- src/FilterSetting/AbstractRange.php
+++ src/FilterSetting/AbstractRange.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/filter_range.
  *
- * (c) 2012-2024 The MetaModels team.
+ * (c) 2012-2025 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,7 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2024 The MetaModels team.
+ * @copyright  2012-2025 The MetaModels team.
  * @license    https://github.com/MetaModels/filter_range/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -364,7 +364,7 @@ abstract class AbstractRange extends Simple
         $value = $this->getParameterValue($arrFilterUrl);
 
         // No filter values, get out.
-        if (!is_array($value)) {
+        if (!\is_array($value) || [] === $value) {
             $objFilter->addFilterRule(new StaticIdList(null));
 
             return;
